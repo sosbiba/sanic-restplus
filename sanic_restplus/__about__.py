95b592fb74635692d8d46a290a17c7c6c9cb440c
Ashley Sommer <ashleysommer@gmail.com>
2017-08-09 15:40:55 +1000

    bump version. And tag.

--- sanic_restplus/__about__.py
+++ sanic_restplus/__about__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.1.1.dev'
+__version__ = '0.1.2dev20170809'
 __description__ = 'Fully featured framework for fast, easy and documented API development with Sanic'
