790265fb7209b172dd52f27b440def0744019fdd
Ashley Sommer <ashleysommer@gmail.com>
2017-10-26 16:13:02 +1000

    Keep order of Accept header value, when parsing from request headers Check all accept values first, before checking for wildcards. Don't want a premature match.

--- sanic_restplus/__about__.py
+++ sanic_restplus/__about__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.1.5dev20171026'
+__version__ = '0.1.6dev20171026'
 __description__ = 'Fully featured framework for fast, easy and documented API development with Sanic'
--- sanic_restplus/utils.py
+++ sanic_restplus/utils.py
@@ -129,7 +129,8 @@ def get_accept_mimetypes(request):
     if accept_types is None:
         return {}
     split_types = str(accept_types).split(',')
-    return {(s, 1): s for s in split_types}
+    # keep the order they appear!
+    return OrderedDict([((s, 1,), s,) for s in split_types])
 
 
 def best_match_accept_mimetype(request, representations, default=None):
@@ -139,15 +140,19 @@ def best_match_accept_mimetype(request, representations, default=None):
         accept_mimetypes = get_accept_mimetypes(request)
         if accept_mimetypes is None or len(accept_mimetypes) < 1:
             return default
+        # find exact matches, in the order they appear in the `Accept:` header
         for accept_type, qual in accept_mimetypes:
-            if accept_type == "*" or accept_type == "*/*" or accept_type == "*.*":
-                return default
             if accept_type in representations:
                 return accept_type
+        # match special types, like "application/json;charset=utf8" where the first half matches.
         for accept_type, qual in accept_mimetypes:
             type_part = str(accept_type).split(';', 1)[0]
             if type_part in representations:
                 return type_part
+        # if _none_ of those don't match, then fallback to wildcard matching
+        for accept_type, qual in accept_mimetypes:
+            if accept_type == "*" or accept_type == "*/*" or accept_type == "*.*":
+                return default
     except (AttributeError, KeyError):
         return default
 
