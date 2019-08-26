0e76e2696e842d9a061bac0197c98481de05aa2c
Ashley Sommer <ashleysommer@gmail.com>
2019-08-26 16:26:39 +1000

    bump to new beta release version

--- sanic_restplus/__about__.py
+++ sanic_restplus/__about__.py
@@ -1,4 +1,4 @@
 # -*- coding: latin-1 -*-
 #
-__version__ = '0.3.5.post1'
+__version__ = '0.4.0b1'
 __description__ = 'Fully featured framework for fast, easy and documented API development with Sanic'
