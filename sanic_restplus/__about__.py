99d8f2af8af4fbfdbbf45b20fd26f9a383a68723
Ashley Sommer <ashleysommer@gmail.com>
2019-08-26 10:47:02 +1000

    bump version for another pypi release

--- sanic_restplus/__about__.py
+++ sanic_restplus/__about__.py
@@ -1,4 +1,4 @@
 # -*- coding: latin-1 -*-
 #
-__version__ = '0.3.5'
+__version__ = '0.3.5.post1'
 __description__ = 'Fully featured framework for fast, easy and documented API development with Sanic'
